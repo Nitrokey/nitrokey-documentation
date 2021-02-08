db176be6e94fb2118d77f441ea61f2c88bcc32cc
sphinx <72864440+nk-translate@users.noreply.github.com>
2021-02-08 12:01:59 +0100

    activated pretty search results plugin

--- conf.py
+++ conf.py
@@ -41,6 +41,7 @@ release = ''
 extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
+    'sphinxprettysearchresults',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
