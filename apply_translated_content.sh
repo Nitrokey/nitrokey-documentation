b74d2315e0d574db085d13a9dd7b3772c26fb5e4
sphinx <72864440+nk-translate@users.noreply.github.com>
2021-03-30 16:55:51 +0200

    changed log file for webhook

--- apply_translated_content.sh
+++ apply_translated_content.sh
@@ -1,4 +1,4 @@
-echo "$(date) [apply_translated_content.sh] webhook Weblate triggered. Building language versions." >> /var/www/sphinx/logs/webhook.log
+echo "$(date) [apply_translated_content.sh] webhook Weblate triggered. Building language versions." >> /var/www/sphinx/logs_sphinx/webhook.log
 
 cd /var/www/sphinx/sphinx/nitrokey-documentation
 
@@ -8,14 +8,14 @@ git pull
 
 # building German version
 sphinx-build -a -D language='de' -b html /var/www/sphinx/sphinx/nitrokey-documentation/ /var/www/sphinx/www/static/de/
-echo "$(date) [apply_translated_content.sh] German DONE." >> /var/www/sphinx/logs/webhook.log
+echo "$(date) [apply_translated_content.sh] German DONE." >> /var/www/sphinx/logs_sphinx/webhook.log
 
 
 # building french version
 #sphinx-build -a -D language='fr' -b html /var/www/sphinx/sphinx/nitrokey-documentation/ /var/www/sphinx/www/static/fr/
-#echo "$(date) [apply_translated_content.sh] french DONE." >> /var/www/sphinx/logs/webhook.log
+#echo "$(date) [apply_translated_content.sh] french DONE." >> /var/www/sphinx/logs_sphinx/webhook.log
 
 # building spanish version
 #sphinx-build -a -D language='es' -b html /var/www/sphinx/sphinx/nitrokey-documentation/ /var/www/sphinx/www/static/es/
-#echo "$(date) [apply_translated_content.sh] spanish DONE." >> /var/www/sphinx/logs/webhook.log
+#echo "$(date) [apply_translated_content.sh] spanish DONE." >> /var/www/sphinx/logs_sphinx/webhook.log
 
