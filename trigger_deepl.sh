a0324014b997ddab18a8f29b03a0bde77ec09f45
sphinx <72864440+nk-translate@users.noreply.github.com>
2022-01-17 16:51:18 +0100

    deepl trigger with json and weblate api

--- trigger_deepl.sh
+++ trigger_deepl.sh
@@ -1,31 +1,26 @@
 languages=("de" "fr" "es" "nl" "it" "ja" "ru" "el" "bg" "da" "et" "fi" "lv" "lt" "pl" "pt" "ro" "sv" "sk" "sl" "cs" "hu")
+components=("documentation-fido2" "documentation-hsm" "documentation-nethsm" "documentation-netbox" "documentation-nitrokey3" "documentation-nitropad" "documentation-nitropc" "documentation-nitrophone" "documentation-pro" "documentation-shared-faq" "documentation-sphinx" "documentation-start" "documentation-storage" "documentation-string" "documentation-syntax" "documentation-t430" "documentation-u2f" "documentation-x230" "glossary" "index")
+
+
+# provide access token from https://translate.nitrokey.com/accounts/profile/#api
+token=$1
 
 for lang in "${languages[@]}"
-do
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-fido2/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-hsm/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-nethsm/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-nextbox/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-nitrokey3/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-nitropad/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-nitropc/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-nitrophone/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-pro/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-shared-faqs/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-sphinx/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-start/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-storage/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-string/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-syntax/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-t430/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-to-be-integrated/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-u2f/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-x230/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/glossary/$lang/autotranslate
-	https://translate.nitrokey.com/api/translations/nitrokey-documentation/index/$lang/autotranslate
+do	
+	for component in "${components[@]}"
+	do
+		curl \
+		--data-binary '{
+			"mode":"translate",
+			"auto_source":"mt",
+			"engines":["deepl"],
+			"filter_type":"nottranslated",
+			"threshold":"80"
+		}' \
+		-H "Content-Type: application/json" \
+		-H "Authorization: Token $token" \
+		https://translate.nitrokey.com/api/translations/nitrokey-documentation/$component/$lang/autotranslate/
+	done
 done
 
 
-
-# curl -H "Authorization: Token QsCSCNtGoe0OIoR6HtMcdo66cLIGXAtxZJljiWzw" https://translate.nitrokey.com/api/translations/nitrokey-documentation/documentation-fido2/et/autotranslate
-# wlc --key QsCSCNtGoe0OIoR6HtMcdo66cLIGXAtxZJljiWzw --url https://translate.nitrokey.com/api/ list-projects
