1f7ac3e4076302a6d9986b8f8d467249a5ccca7f
sphinx <72864440+nk-translate@users.noreply.github.com>
2022-01-17 17:15:17 +0100

    Deepl trigger adjustments

--- trigger_deepl.sh
+++ trigger_deepl.sh
@@ -1,14 +1,26 @@
 languages=("de" "fr" "es" "nl" "it" "ja" "ru" "el" "bg" "da" "et" "fi" "lv" "lt" "pl" "pt" "ro" "sv" "sk" "sl" "cs" "hu")
-components=("documentation-fido2" "documentation-hsm" "documentation-nethsm" "documentation-netbox" "documentation-nitrokey3" "documentation-nitropad" "documentation-nitropc" "documentation-nitrophone" "documentation-pro" "documentation-shared-faq" "documentation-sphinx" "documentation-start" "documentation-storage" "documentation-string" "documentation-syntax" "documentation-t430" "documentation-u2f" "documentation-x230" "glossary" "index")
+components=("documentation-fido2" "documentation-hsm" "documentation-nethsm" "documentation-netbox" "documentation-nitrokey3" "documentation-nitropad" "documentation-nitropc" "documentation-nitrophone" "documentation-pro" "documentation-shared-faq" "documentation-sphinx" "documentation-start" "documentation-storage" "documentation-syntax" "documentation-t430" "documentation-u2f" "documentation-x230" "glossary" "index")
 
 
 # provide access token from https://translate.nitrokey.com/accounts/profile/#api
 token=$1
 
+
+if [ $# -eq 0 ]
+  then
+	echo "Please provide your Weblate Access Token as first Parameter. Get it from https://translate.nitrokey.com/accounts/profile/#api"
+	exit
+fi
+
+
+
 for lang in "${languages[@]}"
-do	
+
+do
+	echo -e "\nStarting deepl Translation for language $lang..."
 	for component in "${components[@]}"
 	do
+		echo -e "Starting to translate $lang for component $component"
 		curl \
 		--data-binary '{
 			"mode":"translate",
@@ -20,7 +32,9 @@ do
 		-H "Content-Type: application/json" \
 		-H "Authorization: Token $token" \
 		https://translate.nitrokey.com/api/translations/nitrokey-documentation/$component/$lang/autotranslate/
+		echo -e "\ndone"
 	done
+	echo -e "\nLanguage $lang passed"
 done
 
 
