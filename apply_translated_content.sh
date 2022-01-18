063f7f85117d5efe5968e68c23e6840ccf423eac
sphinx <72864440+nk-translate@users.noreply.github.com>
2022-01-18 14:33:37 +0100

    nebenläufige Build Prozesse verhindern

--- apply_translated_content.sh
+++ apply_translated_content.sh
@@ -30,19 +30,23 @@ echo "$(date) [apply_translated_content.sh] Building language versions." >> /var
 
 for lang in "${languages[@]}"
 do
-	echo "$(date) [apply_translated_content.sh] (SPHINX) Building Language Version $lang..." >> /var/www/sphinx/logs_sphinx/webhook.log
-
-	sphinx-build -a -D language="$lang" -b html /var/www/sphinx/sphinx/nitrokey-documentation/ /var/www/sphinx/www/docs.nitrokey.com_$lang-temp
-	status=$?
-
-	if [ $status -eq 0 ]
-	then
-		echo "$(date) [apply_translated_content.sh] (SPHINX) Building Language Version $lang...DONE" >> /var/www/sphinx/logs_sphinx/webhook.log
-		rm -r  /var/www/sphinx/www/static/$lang
-		mv /var/www/sphinx/www/docs.nitrokey.com_$lang-temp /var/www/sphinx/www/static/$lang
+	if [ -d "/var/www/sphinx/www/docs.nitrokey.com_$lang-temp" ]; then
+		echo "$(date) [apply_translated_content.sh] (SPHINX) Building Language Version $lang SKIPPED. (Directory not empty. Another build process may be running)"
 	else
-		echo echo "FAILED" >> /var/www/sphinx/logs_sphinx/webhook.log
-		echo "Building Docs.nitrokey.com Language $lang FAILED. The online version was as not altered." | mail -s "[Sphinx] Building Language $lang FAILED." $admin_mail_address
+		echo "$(date) [apply_translated_content.sh] (SPHINX) Building Language Version $lang..." >> /var/www/sphinx/logs_sphinx/webhook.log
+
+		sphinx-build -a -D language="$lang" -b html /var/www/sphinx/sphinx/nitrokey-documentation/ /var/www/sphinx/www/docs.nitrokey.com_$lang-temp
+		status=$?
+	
+		if [ $status -eq 0 ]
+		then
+			echo "$(date) [apply_translated_content.sh] (SPHINX) Building Language Version $lang...DONE" >> /var/www/sphinx/logs_sphinx/webhook.log
+			rm -r  /var/www/sphinx/www/static/$lang
+			mv /var/www/sphinx/www/docs.nitrokey.com_$lang-temp /var/www/sphinx/www/static/$lang
+		else
+			echo echo "FAILED" >> /var/www/sphinx/logs_sphinx/webhook.log
+			echo "Building Docs.nitrokey.com Language $lang FAILED. The online version was as not altered." | mail -s "[Sphinx] Building Language $lang FAILED." $admin_mail_address
+		fi
 	fi
 done
 
