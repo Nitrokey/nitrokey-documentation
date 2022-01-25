afd0bfa179d9db441fcd273aba26262b981233c2
sphinx <72864440+nk-translate@users.noreply.github.com>
2022-01-26 00:13:12 +0100

    Language Files generated by Sphinx (513515)

--- apply_singlelanguage.sh
+++ apply_singlelanguage.sh
@@ -3,15 +3,15 @@
 lang=$1 
 	echo "$(date) [apply_translated_content.sh] (SPHINX) Building Language Version $lang..." >> /var/www/sphinx/logs_sphinx/webhook.log
 
-	sphinx-build -a -D language="$lang" -b html /var/www/sphinx/sphinx/nitrokey-documentation/ /var/www/sphinx/www/docs.nitrokey.com_$lang-temp
+	sphinx-build -a -D language="$lang" -b html /var/www/sphinx/sphinx/nitrokey-documentation/  /var/www/sphinx/sphinx/sphinx_build_temp/$lang-temp
 	status=$?
 
 	if [ $status -eq 0 ]
 	then
 		echo "$(date) [apply_translated_content.sh] (SPHINX) Building Language Version $lang...DONE" >> /var/www/sphinx/logs_sphinx/webhook.log
 		rm -r  /var/www/sphinx/www/static/$lang
-		mv /var/www/sphinx/www/docs.nitrokey.com_$lang-temp /var/www/sphinx/www/static/$lang
+		cp -r /var/www/sphinx/sphinx/sphinx_build_temp/$lang-temp /var/www/sphinx/www/static/$lang
 	else
-		echo echo "FAILED" >> /var/www/sphinx/logs_sphinx/webhook.log
+		echo "FAILED" >> /var/www/sphinx/logs_sphinx/webhook.log
 	fi
 
